be6b55411f29c244fb3be1007892434af0112caf
GiovannaLyssa99 <giovannalyssa@discente.ufg.br>
2023-05-31 20:13:17 -0300

    Adição de mais classes

--- T07/src/main/java/com/github/giovannalyssa99/ufg/poo/Pessoa.java
+++ T07/src/main/java/com/github/giovannalyssa99/ufg/poo/Pessoa.java
@@ -1,5 +1,8 @@
 package com.github.giovannalyssa99.ufg.poo;
 
 public class Pessoa {
-    
+    private Data nascimento;
+    private String nome;
+    private Pessoa pai;
+    private Pessoa mae;
 }
